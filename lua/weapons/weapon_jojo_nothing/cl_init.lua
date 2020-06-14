dba3f549dee92991a715ae5a7444c91e4636a932
Cpt. Hazama <Caleb123459@gmail.com>
2020-06-13 21:14:01 -0400

    Very illogical issue that required an illogical fix

--- lua/weapons/weapon_jojo_nothing/cl_init.lua
+++ lua/weapons/weapon_jojo_nothing/cl_init.lua
@@ -1,13 +1,13 @@
 include('shared.lua')
 
-SWEP.PrintName        = "No Weapons"
+SWEP.PrintName        = ""
 SWEP.Author           = ""
 SWEP.Purpose          = ""
 SWEP.Instructions     = ""
 SWEP.Slot             = 0
 SWEP.SlotPos          = 0
 SWEP.DrawAmmo         = false
-SWEP.DrawCrosshair    = false
+SWEP.DrawCrosshair    = true
 
 function SWEP:ShouldDrawViewModel()
 	return false
